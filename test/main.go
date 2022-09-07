ead271f210146b61b862c7d7fa935437285db116
lmatheo <matheo.lang@ynov.com>
2022-09-07 14:46:03 +0200

    preintcomb

--- test/main.go
+++ test/main.go
@@ -2,8 +2,12 @@ package main
 
 import "piscine"
 
+// func main() {
+// 	piscine.IsNegative(1)
+// 	piscine.IsNegative(0)
+// 	piscine.IsNegative(-1)
+// }
+
 func main() {
-	piscine.IsNegative(1)
-	piscine.IsNegative(0)
-	piscine.IsNegative(-1)
+	piscine.PrintComb()
 }
