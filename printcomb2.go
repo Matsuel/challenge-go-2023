2420f91205941f040f821df425a4ecd3160663d6
lmatheo <matheo.lang@ynov.com>
2022-09-07 15:34:37 +0200

    printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -6,7 +6,7 @@ import (
 
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
-		for j := '0'; j <= '8'; j++ {
+		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
 					if string(i)+string(j) < string(k)+string(l) {
