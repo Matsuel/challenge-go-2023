b5b74265dd27d973de353e632e52cafb67348ae0
lmatheo <matheo.lang@ynov.com>
2022-09-12 10:31:07 +0200

    iterative factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,7 +4,7 @@ func IterativeFactorial(nb int) int {
 	rep := 1
 	if nb < 0 || nb >= 20 {
 		return 0
-	} else if nb == 1 || nb == 2 {
+	} else if nb == 1 {
 		return 1
 	} else {
 		for i := nb; i > 0; i-- {
