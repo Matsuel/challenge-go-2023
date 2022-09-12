d55865381c8f054f4caf52b590ab76834a750d7c
lmatheo <matheo.lang@ynov.com>
2022-09-12 11:02:29 +0200

    iterative factorial

--- iterativepower.go
+++ iterativepower.go
@@ -4,7 +4,7 @@ func IterativePower(nb int, power int) int {
 	rep := nb
 	if nb < 0 {
 		return 1
-	} else if power == 0 && nb == 0 {
+	} else if power <= 0 && nb <= 0 {
 		return 1
 	} else if power < 0 {
 		return 0
