b0e339328f52f5b3414a35b78d4081de6ddea130
lmatheo <matheo.lang@ynov.com>
2022-09-12 10:29:15 +0200

    iterative factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,9 +2,9 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	rep := 1
-	if nb < 0 {
+	if nb < 0 || nb >= 10 {
 		return 0
-	} else if nb > 10 || nb == 1 {
+	} else if nb == 1 {
 		return 1
 	} else {
 		for i := nb; i > 0; i-- {
