753beacfe5e74cf8edb02a9bf18c8a77fef1bef7
lmatheo <matheo.lang@ynov.com>
2022-09-12 11:40:20 +0200

    recursivepower

--- sqrt.go
+++ sqrt.go
@@ -2,9 +2,9 @@ package piscine
 
 func Sqrt(nb int) int {
 	rep := 0
-	if nb < 0 {
+	if nb <= 0 {
 		return 0
-	} else if nb == 1 || nb == 0 {
+	} else if nb == 1 {
 		return 1
 	} else {
 		for i := 1; i < nb; i++ {
