cbfa1f28cc7735b6853426d77f74a8f06d63ad63
lmatheo <matheo.lang@ynov.com>
2022-09-12 11:04:53 +0200

    iterative factorial

--- iterativepower.go
+++ iterativepower.go
@@ -2,12 +2,10 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 	rep := nb
-	if nb < 0 {
-		return 1
-	} else if power <= 0 && nb <= 0 {
-		return 1
-	} else if power < 0 {
+	if power < 0 {
 		return 0
+	} else if power == 0 && nb == 0 {
+		return 1
 	}
 	for i := power; i > 1; i-- {
 		rep *= nb
