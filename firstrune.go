4804c3678b786aa1ddec81424e991eb83c65bd2d
lmatheo <matheo.lang@ynov.com>
2022-09-15 09:42:00 +0200

    firstrune

--- firstrune.go
+++ firstrune.go
@@ -1,7 +1,7 @@
 package piscine
 
 func FirstRune(s string) rune {
-	if rune(s[0]) <= 122 && rune(s[0]) > 97 {
+	if rune(s[0]) <= 122 && rune(s[0]) > 97 || rune(s[0]) <= 57 && rune(s[0]) > 47 {
 		return (rune(s[0]))
 	} else {
 		return (rune(s[0]) - 130)
