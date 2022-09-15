30076d2a7a830c80c6ef8876c670ec8794d6d153
lmatheo <matheo.lang@ynov.com>
2022-09-15 14:45:24 +0200

    join

--- join.go
+++ join.go
@@ -2,9 +2,13 @@ package piscine
 
 func Join(strs []string, sep string) string {
 	var rep string
-	for _, el := range strs {
-		rep += string(el)
-		rep += sep
+	for i := 0; i < len(strs); i++ {
+		if i != len(strs)-1 {
+			rep += string(strs[i])
+			rep += sep
+		} else {
+			rep += string(strs[i])
+		}
 	}
 	return rep
 }
