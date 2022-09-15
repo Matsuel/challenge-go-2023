802d234608b2de4dcc3352e24cdb7baa7c1ad70c
lmatheo <matheo.lang@ynov.com>
2022-09-15 16:52:39 +0200

    printnbrinorder

--- trimatoi.go
+++ trimatoi.go
@@ -1,13 +1,16 @@
 package piscine
 
 func TrimAtoi(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
 	rep := 0
 	m := 1
 	for _, ch := range s {
 		if IsNumeric(string(ch)) {
 			rep += int(ch - 48)
 			rep *= 10
-		} else if ch == '-' && rep != 0 {
+		} else if ch == '-' && rep != 10 {
 			m = -1
 		}
 	}
