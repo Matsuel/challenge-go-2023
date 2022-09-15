731409dc8bc1abb7828da1aa29a81063d280b514
lmatheo <matheo.lang@ynov.com>
2022-09-15 11:42:25 +0200

    firstrune

---
+++ isalpha.go
@@ -0,0 +1,10 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	for _, ch := range s {
+		if rune(ch) < 65 || rune(ch) > 90 || rune(ch) < 97 || rune(ch) > 122 || rune(ch) < 48 || rune(ch) > 57 {
+			return false
+		}
+	}
+	return true
+}
