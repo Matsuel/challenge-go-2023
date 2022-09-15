5e1465ea48deee05361fe291fb3f2d6861acb830
lmatheo <matheo.lang@ynov.com>
2022-09-15 14:26:20 +0200

    isprintable

--- capitalize.go
+++ capitalize.go
@@ -4,14 +4,12 @@ func Capitalize(s string) string {
 	rep := ""
 	for i := 0; i < len(s); i++ {
 		if i == 0 && IsLower(string(s[i])) {
-			b := rune(s[i] - 32)
-			rep += string(b)
+			rep += string(rune(s[i] - 32))
 		} else if IsLower(string(s[i])) && (!IsAlpha(string(s[i-1]))) {
-			b := rune(s[i] - 32)
-			rep += string(b)
+			rep += string(rune(s[i] - 32))
 		} else if i == 0 && IsUpper(string(s[i])) {
 			rep += string(rune(s[i]))
-		} else if IsUpper(string(s[i])) && IsUpper(string(s[i-1])) {
+		} else if IsUpper(string(s[i])) && IsUpper(string(rep[i-1])) {
 			rep += string(rune(s[i] + 32))
 		} else {
 			rep += string(s[i])
