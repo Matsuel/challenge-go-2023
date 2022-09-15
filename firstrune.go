583736d21c64cfa94445397a4abf50635d1034cc
lmatheo <matheo.lang@ynov.com>
2022-09-15 09:42:42 +0200

    firstrune

--- firstrune.go
+++ firstrune.go
@@ -1,7 +1,7 @@
 package piscine
 
 func FirstRune(s string) rune {
-	if rune(s[0]) <= 122 && rune(s[0]) > 97 || rune(s[0]) <= 57 && rune(s[0]) > 47 {
+	if rune(s[0]) <= 122 && rune(s[0]) > 97 || rune(s[0]) <= 57 && rune(s[0]) > 47 || rune(s[0]) <= 90 && rune(s[0]) > 64 {
 		return (rune(s[0]))
 	} else {
 		return (rune(s[0]) - 130)
