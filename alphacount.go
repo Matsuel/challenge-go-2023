739778b9770c981345eb5378ae8b0e7f43e8e726
lmatheo <matheo.lang@ynov.com>
2022-09-15 10:03:28 +0200

    firstrune

--- alphacount.go
+++ alphacount.go
@@ -1,9 +1,9 @@
 package piscine
 
 func AlphaCount(s string) int {
-	cpt:=0
-	for _, ch:= range s{
-		if rune(ch)>=65 && rune(ch)<=90 || rune(ch)>=97 && rune(ch)<=122{
+	cpt := 0
+	for _, ch := range s {
+		if rune(ch) >= 65 && rune(ch) <= 90 || rune(ch) >= 97 && rune(ch) <= 122 {
 			cpt++
 		}
 	}
