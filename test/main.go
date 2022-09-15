c96b70528d94fba5cfc78cf7a8afe4194dc69e21
lmatheo <matheo.lang@ynov.com>
2022-09-15 09:47:45 +0200

    firstrune

--- test/main.go
+++ test/main.go
@@ -316,6 +316,21 @@
 // 	z01.PrintRune('\n')
 // }
 
+// package main
+
+// import (
+// 	"piscine"
+
+// 	"github.com/01-edu/z01"
+// )
+
+// func main() {
+// 	z01.PrintRune(piscine.LastRune("Hello!"))
+// 	z01.PrintRune(piscine.LastRune("Salut!"))
+// 	z01.PrintRune(piscine.LastRune("Ola!"))
+// 	z01.PrintRune('\n')
+// }
+
 package main
 
 import (
@@ -325,8 +340,10 @@ import (
 )
 
 func main() {
-	z01.PrintRune(piscine.LastRune("Hello!"))
-	z01.PrintRune(piscine.LastRune("Salut!"))
-	z01.PrintRune(piscine.LastRune("Ola!"))
+	z01.PrintRune(piscine.NRune("Hello!", 3))
+	z01.PrintRune(piscine.NRune("Salut!", 2))
+	z01.PrintRune(piscine.NRune("Bye!", -1))
+	z01.PrintRune(piscine.NRune("Bye!", 5))
+	z01.PrintRune(piscine.NRune("Ola!", 4))
 	z01.PrintRune('\n')
 }
