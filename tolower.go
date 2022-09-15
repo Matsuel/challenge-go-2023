07d752c95e6f2600fcf01f22dd98cdf18c9db13d
lmatheo <matheo.lang@ynov.com>
2022-09-15 12:02:31 +0200

    isprintable

--- tolower.go
+++ tolower.go
@@ -4,7 +4,7 @@ func ToLower(s string) string {
 	var rep string
 	for _, ch := range s {
 		if IsUpper(string(ch)) {
-			rep += string(rune(ch - 32))
+			rep += string(rune(ch + 32))
 		} else {
 			rep += string(ch)
 		}
