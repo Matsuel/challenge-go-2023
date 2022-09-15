2033e3b48ada1918fad6a7ca591cbf7ed2ec5795
lmatheo <matheo.lang@ynov.com>
2022-09-15 12:02:08 +0200

    isprintable

---
+++ tolower.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ToLower(s string) string {
+	var rep string
+	for _, ch := range s {
+		if IsUpper(string(ch)) {
+			rep += string(rune(ch - 32))
+		} else {
+			rep += string(ch)
+		}
+	}
+	return rep
+}
