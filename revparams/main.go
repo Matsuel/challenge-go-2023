a30f4ecaa1a2fd0034a25d92078886013703c9df
lmatheo <matheo.lang@ynov.com>
2022-09-16 17:09:33 +0200

    revparams

---
+++ revparams/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arguments := os.Args
+	for i := len(arguments); i > 1; i-- {
+		for j := 0; j < len(string(arguments[i])); j++ {
+			z01.PrintRune(rune(arguments[i][j]))
+		}
+		z01.PrintRune('\n')
+	}
+}
