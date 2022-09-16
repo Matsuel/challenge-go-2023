32760a1a62dc7c78470cdf114da48bd41d1664c3
lmatheo <matheo.lang@ynov.com>
2022-09-16 17:06:21 +0200

    printparams

--- printparams/main.go
+++ printparams/main.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	arguments := os.Args[0]
-	for i := 0; i < len(arguments); i++ {
+	arguments := os.Args
+	for i := 1; i < len(arguments); i++ {
 		for j := 0; j < len(string(arguments[i])); j++ {
-			z01.PrintRune(arguments[i][j])
+			z01.PrintRune(rune(arguments[i][j]))
 		}
 		z01.PrintRune('\n')
 	}
