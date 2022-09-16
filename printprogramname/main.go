1fb64630a81a6ba37e7c34fe35571518e83feb8e
lmatheo <matheo.lang@ynov.com>
2022-09-16 16:28:12 +0200

    print programme

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -9,17 +9,9 @@ import (
 func main() {
 	arguments := os.Args[0]
 	for _, el := range arguments {
-		z01.PrintRune(el)
+		if (el != rune(92)) && (el != rune(46)) && (el != rune(47)) {
+			z01.PrintRune(el)
+		}
 	}
-	// var elem string
-	// for _, el := range arguments {
-	// 	if el == rune(92) {
-	// 		elem = ""
-	// 	} else {
-	// 		elem += string(el)
-	// 	}
-	// }
-	// for i := 0; (rune(elem[i]) != rune(46)) || i < len(elem)-1; i++ {
-	// 	z01.PrintRune(rune(elem[i]))
-	// }
+	// si sa print toute la ligne c'est normal c'est windows
 }
