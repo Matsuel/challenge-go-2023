ef881d7d076b8fcd9cc7682b8ef205a5b5636f1b
lmatheo <matheo.lang@ynov.com>
2022-09-16 17:11:45 +0200

    revparams

--- revparams/main.go
+++ revparams/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	arguments := os.Args
-	for i := len(arguments); i > 2; i-- {
+	for i := len(arguments) - 1; i > 1; i-- {
 		for j := 0; j < len(string(arguments[i])); j++ {
 			z01.PrintRune(rune(arguments[i][j]))
 		}
