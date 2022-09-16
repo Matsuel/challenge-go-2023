3387c1d10e905be3ef7f2f74e60e75333f47902e
lmatheo <matheo.lang@ynov.com>
2022-09-16 16:24:13 +0200

    print programme

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -8,15 +8,18 @@ import (
 
 func main() {
 	arguments := os.Args[0]
-	var elem string
 	for _, el := range arguments {
-		if el == rune(92) {
-			elem = ""
-		} else {
-			elem += string(el)
-		}
+		z01.PrintRune(el)
 	}
-	for i := 0; (rune(elem[i]) != rune(46)) || i < len(elem)-1; i++ {
-		z01.PrintRune(rune(elem[i]))
-	}
+	// var elem string
+	// for _, el := range arguments {
+	// 	if el == rune(92) {
+	// 		elem = ""
+	// 	} else {
+	// 		elem += string(el)
+	// 	}
+	// }
+	// for i := 0; (rune(elem[i]) != rune(46)) || i < len(elem)-1; i++ {
+	// 	z01.PrintRune(rune(elem[i]))
+	// }
 }
