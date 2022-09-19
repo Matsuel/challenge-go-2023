00d5aba9cd7f1c75f5bfab261d088125ab8dd109
lmatheo <matheo.lang@ynov.com>
2022-09-19 14:59:14 +0200

    appendrange

--- appendrange.go
+++ appendrange.go
@@ -1,10 +1,8 @@
 package piscine
 
 func AppendRange(min, max int) []int {
-	rep := []int{}
-	if min >= max {
-		rep = []int(nil)
-	} else if min < max {
+	rep := []int(nil)
+	if min < max {
 		for i := min; i < max; i++ {
 			rep = append(rep, i)
 		}
