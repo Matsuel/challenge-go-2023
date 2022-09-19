cd54b937f31b4e8bc673416e9fd710c64bc14042
lmatheo <matheo.lang@ynov.com>
2022-09-19 11:04:13 +0200

    nbrconvertalpha

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -8,8 +8,6 @@ import (
 )
 
 func main() {
-	// lettre := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-	// corr_lettre := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"}
 	arguments := os.Args
 	if arguments[1] == "--upper" {
 		for i := 2; i < len(arguments); i++ {
@@ -34,6 +32,4 @@ func main() {
 			}
 		}
 	}
-	// var sol int
-
 }
