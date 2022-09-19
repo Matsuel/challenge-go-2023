bf5329abb4686296d1f762783b8b0d6d626c1644
lmatheo <matheo.lang@ynov.com>
2022-09-19 11:11:51 +0200

    nbrconvertalpha

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -11,7 +11,7 @@ func main() {
 	if len(arguments) == 0 {
 		z01.PrintRune(rune(32))
 		z01.PrintRune('\n')
-	} else if arguments[1] == "--upper" {
+	} else if len(arguments) > 1 && arguments[1] == "--upper" {
 		for i := 2; i < len(arguments); i++ {
 			a := Atoi(arguments[i])
 			if a == 0 {
