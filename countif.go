1fb4691765a21f56e968d44a31299f9f454d0ce3
lmatheo <matheo.lang@ynov.com>
2022-09-26 15:18:10 +0200

    cat

--- countif.go
+++ countif.go
@@ -1,10 +1,10 @@
 package piscine
 
 func CountIf(f func(string) bool, tab []string) int {
-	rep:=0
-	for i:=0; i<len(tab); i++{
-		if f(tab[i])== true{
-			rep+=1
+	rep := 0
+	for i := 0; i < len(tab); i++ {
+		if f(tab[i]) == true {
+			rep += 1
 		}
 	}
 	return rep
