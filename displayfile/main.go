fce472f4b4af6b19ec1ddb29810019c861aea5c4
lmatheo <matheo.lang@ynov.com>
2022-09-26 14:15:20 +0200

    displayfile

---
+++ displayfile/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	arg := os.Args
+	if len(arg) == 1 {
+		fmt.Println("File name missing")
+	} else if len(arg) == 2 {
+		data, err := ioutil.ReadFile(arg[1])
+		if err == nil {
+			fmt.Printf(string(data[2:]))
+		}
+	} else {
+		fmt.Println("Too many arguments")
+	}
+}
