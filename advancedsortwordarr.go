cccb71092b0268cc401483abe05e771937321836
lmatheo <matheo.lang@ynov.com>
2022-09-27 13:40:44 +0200

    sortword

--- advancedsortwordarr.go
+++ advancedsortwordarr.go
@@ -2,8 +2,10 @@ package piscine
 
 func AdvancedSortWordArr(a []string, f func(a, b string) int) {
 	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) == 1 {
-			a[i], a[i+1] = a[i+1], a[i]
+		for j := i + 1; j < len(a); j++ {
+			if f(a[i], a[i+1]) > 0 {
+				a[i], a[i+1] = a[i+1], a[i]
+			}
 		}
 	}
 }
