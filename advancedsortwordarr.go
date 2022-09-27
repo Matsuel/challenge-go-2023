73dfea9f1c21bdc4a0c74a2aeffd27ede6b1bd47
lmatheo <matheo.lang@ynov.com>
2022-09-27 13:42:23 +0200

    sortword

--- advancedsortwordarr.go
+++ advancedsortwordarr.go
@@ -2,9 +2,9 @@ package piscine
 
 func AdvancedSortWordArr(a []string, f func(a, b string) int) {
 	for i := 0; i < len(a); i++ {
-		for j := 0; j < len(a); j++ {
-			if f(a[i], a[i+1]) > 0 {
-				a[i], a[i+1] = a[i+1], a[i]
+		for j := i + 1; j < len(a); j++ {
+			if f(a[i], a[j]) > 0 {
+				a[i], a[j] = a[j], a[i]
 			}
 		}
 	}
