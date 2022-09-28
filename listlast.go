5776125b3468634bd67336b5f0235088752d9800
lmatheo <matheo.lang@ynov.com>
2022-09-28 14:29:37 +0200

    ztail

--- listlast.go
+++ listlast.go
@@ -1,5 +1,14 @@
 package piscine
 
 func ListLast(l *List) interface{} {
-	l.Head = nil
+	if l.Head == nil {
+		return nil
+	}
+	for l.Head != nil {
+		if l.Head.Next == nil {
+			return l.Head.Data
+		}
+		l.Head = l.Head.Next
+	}
+	return l.Head.Data
 }
