b55c7132d46f391c7f637ded76ff40093b6e0cf3
lmatheo <matheo.lang@ynov.com>
2022-09-28 16:27:07 +0200

    listinsert

--- sortlistinsert.go
+++ sortlistinsert.go
@@ -1,11 +1,9 @@
 package piscine
 
 func SortListInsert(l *NodeI, data_ref int) *NodeI {
-
 	n := &NodeI{}
 	n.Data = data_ref
 	n.Next = nil
-
 	if l == nil || l.Data >= n.Data {
 		n.Next = l
 		return n
