253c72a5910ed91e3edacb7e7c34a5170f80e616
lmatheo <matheo.lang@ynov.com>
2022-09-28 11:37:40 +0200

    ztail

--- ztail/main.go
+++ ztail/main.go
@@ -79,3 +79,29 @@ func main() {
 		fi.Close()
 	}
 }
+
+func Atoi(s string) int {
+	atoi := 0
+	for _, ch := range s {
+		if ch >= '0' && ch <= '9' {
+			atoi = atoi*10 + int(ch-'0')
+		} else {
+			return 0
+		}
+	}
+	return atoi
+}
+
+func Itoa(i int) string {
+	itoa := ""
+	for i != 0 {
+		ch := i % 10
+		i /= 10
+		itoa += string(ch + '0')
+	}
+	res := ""
+	for i := len(itoa) - 1; i >= 0; i-- {
+		res += string(itoa[i])
+	}
+	return res
+}
