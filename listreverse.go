aaae5ad7479fde49f0b101cd6982f223c43b6e15
lmatheo <matheo.lang@ynov.com>
2022-09-28 14:43:12 +0200

    listclear

---
+++ listreverse.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ListReverse(l *List) {
+	noeud_courant := l.Head
+	r := nil
+	for noeud_courant != nil {
+		noeud_suivant := noeud_courant.Next
+		noeud_courant.Next = r
+		r = noeud_courant
+		noeud_courant = noeud_suivant
+	}
+	l.Head, l.Tail = l.Tail, l.Head
+}
