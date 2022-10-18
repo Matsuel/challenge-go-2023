68f1b0e9ea61cad1e33760faf37adb67f97d6d9f
lmatheo <matheo.lang@ynov.com>
2022-10-18 10:01:52 +0200

    strrev

--- strrev.go
+++ strrev.go
@@ -1,22 +1,16 @@
-// package piscine
-
-// func StrRev(s string) string {
-// 	var word string
-// 	word_list := []string{}
-// 	for _, character := range s {
-// 		word_list = append(word_list, string(character))
-// 	}
-// 	for i := len(word_list) - 1; len(word_list) != 0; i-- {
-// 		word += word_list[i]
-// 		word_list = word_list[:len(word_list)-1]
-// 	}
-// 	return word
-// }
-
-// // Stirng c'est comme un tebaleau donc string[-1]= tableau[-1]
-
 package piscine
 
 func StrRev(s string) string {
-	return s
+	var word string
+	word_list := []string{}
+	for _, character := range s {
+		word_list = append(word_list, string(character))
+	}
+	for i := len(word_list) - 1; len(word_list) != 0; i-- {
+		word += word_list[i]
+		word_list = word_list[:len(word_list)-1]
+	}
+	return word
 }
+
+// Stirng c'est comme un tebaleau donc string[-1]= tableau[-1]
